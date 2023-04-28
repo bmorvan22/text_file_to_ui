38fcdd8a5d16f6936428aced744280b9db38ccd0
Baptiste Morvan <baptiste.morvan@epitech.eu>
2023-04-28 18:18:25 +0200

    #3 Added User class

---
+++ text_file_to_ui/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace text_file_to_ui
+{
+    class User
+    {
+        public string name;
+        public string surname;
+        public int birthYear;
+        public string city;
+        public string faculty;
+        public string role;
+        public string specificAttribute;
+    }
+}
